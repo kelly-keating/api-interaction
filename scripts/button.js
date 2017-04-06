19f85ca3fc77b734cb187336f0987586105cdd45
Kelly Keating <kelz@Kellys-MacBook.local>
2017-04-07 04:15:29 +1200

    tweet button added

--- scripts/button.js
+++ scripts/button.js
@@ -3,10 +3,12 @@
 
 var quote = document.getElementById("quote");
 var translation = document.getElementById("translation");
-var btn = document.getElementById("refresh");
+var quoteBtn = document.getElementById("refresh");
+var tweetBtn = document.getElementById("tweet");
 
 document.addEventListener('DOMContentLoaded', generateNewQuote);
-btn.addEventListener('click', generateNewQuote);
+quoteBtn.addEventListener('click', generateNewQuote);
+tweetBtn.addEventListener('click', tweetIt);
 
 function generateNewQuote(){
   var xhr = new XMLHttpRequest();
@@ -22,3 +24,14 @@ function generateNewQuote(){
   quote.innerHTML = json.source;
   translation.innerHTML = json.translation;
 }
+
+
+
+function tweetIt () {
+  var phrase = document.getElementById('quote').innerText;
+  var tweetUrl = 'https://twitter.com/share?text=' +
+    encodeURIComponent(phrase);
+  window.open(tweetUrl);
+}
+//https://dev.twitter.com/web/tweet-button
+//http://ask.metafilter.com/275194/Creating-a-Twitter-button-that-tweets-dynamic-text
